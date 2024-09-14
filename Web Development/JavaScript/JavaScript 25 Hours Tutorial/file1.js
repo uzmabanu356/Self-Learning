6163c82f906ac9ee3a826847f961953c067e1858
Uzma Banu <uzmabanu356@gmail.com>
2024-09-14 20:23:53 +0530

    Add new file

---
+++ Web Development/JavaScript/JavaScript 25 Hours Tutorial/file1.js
@@ -0,0 +1,36 @@
+ /*
+ =========================================Notes================================================
+
+  •  Javascript me hum string print karane k liye single quote, double quote or backtiks ka use kar
+    sakte he.
+  
+    • Javascript me backtick ki help se bhi string print ki jati he. 
+  
+  • JavaScript me semicolon optional hota he. lekin hame use karna chahiye achchi practice
+    mani jati he
+
+*/
+
+"use strict";
+
+console.log("Use Double quote with semicolon ;");
+console.log('Use single quote with semicolon ;');
+console.log(`Use backtick with semicolon ;`);
+console.log("uzma is a very cuite girl")
+
+var myName = "Uzzi";
+console.log("Use Double quote without semicolon ;")
+console.log('Use single quote without semicolon ;')
+console.log(`Use backtick without semicolon ${myName} x;`)
+
+// var let const
+
+var val1 = 5;
+let val2 = 8;
+const val3 = 9;
+val4 = 10;
+
+console.log(val1);
+console.log(val2);
+console.log(val3);
+console.log(val4);
