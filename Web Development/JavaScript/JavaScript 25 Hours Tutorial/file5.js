38e07b8e09b519a28c4ffbba3766547eda404fd7
Uzma Banu <uzmabanu356@gmail.com>
2024-09-25 19:42:34 +0530

    Add new file

---
+++ Web Development/JavaScript/JavaScript 25 Hours Tutorial/file5.js
@@ -0,0 +1,36 @@
+/* ======================================NOTES======================================================
+=================================================================================================
+
+typeof operator:-
+              typeof operator JavaScript mein kisi bhi value ya variable ka type (data type) jaan'ne ke liye use hota hai. Yeh hume batata hai ki variable kis type ka data store kar raha hai, jaise number, string, object, etc.
+
+Convert nmber to string:-
+                    Number ko string me bdlne ke liye variable ke bad  {+" "} lagate he.
+
+Convert string to number:-
+                    String se pehle + lagane se string nnumber me bal jati he.
+*/
+
+// let age =19; //example of typeof operator
+
+// let firstName="Uzma"; //
+
+// console.log(typeof age); //print data type of age
+
+// console.log(typeof firstName); //print data type of name
+
+
+
+
+// let age=19; // example of Convert nmber to string
+// age=age+" " ;
+// console.log (typeof (age));
+
+
+
+
+// let mystr= +"Uzma"; //example of Convert string to number
+// console.log(typeof mystr);
+
+
+
