4bb958f6c9375cd07c30cb86dfbb586f1ca133c2
Uzma Banu <uzmabanu356@gmail.com>
2025-05-19 22:59:52 +0530

    add file

---
+++ Web Development/Learning-With-Faizan/practice ques/array1.js
@@ -0,0 +1,9 @@
+let users = [
+    {name:"amit"},
+    {name:"ravi"},
+    {name:"neha"}
+ ]
+ users.forEach(function(user) {
+   console.log(`hello ${user.name}`)
+ });
+ 
---
+++ Web Development/Learning-With-Faizan/practice ques/array2.js
@@ -0,0 +1,5 @@
+let number=[1,2,3,4,5];
+
+number.forEach(function(num,index){
+console.log(`index ${index} num ${num}`);
+});
