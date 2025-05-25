cfecf770a9dd43c2e971c032b8f3f23488c69147
Uzma Banu <uzmabanu356@gmail.com>
2025-05-25 22:57:39 +0530

    add new file

--- Web Development/JavaScript/JavaScript 25 Hours Tutorial/file10.js
+++ Web Development/JavaScript/JavaScript 25 Hours Tutorial/file10.js
@@ -1,4 +1,13 @@
 // -----------------------------------------------Arrays-------------------------------------
-let array1=['item1','item2'];
-console.log(array1);
-console.log(Array.isArray(array1));
+// let array1=['item1','item2'];
+// console.log(array1);
+// console.log(Array.isArray(array1));
+
+// ----------------Array With forEach loop----------------
+
+let marks = [40, 60, 80];
+if (marks >= 55) {
+marks.forEach(function(mark){
+    console.log("pass");
+});
+}
